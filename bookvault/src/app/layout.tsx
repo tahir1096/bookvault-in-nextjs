9ddd7a43ab1527228f007d712217b4399112db3d
Muhammad Tahir <mtahirkaam@gmail.com>
2025-09-26 20:07:06 +0500

    layout children

--- bookvault/src/app/layout.tsx
+++ bookvault/src/app/layout.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
-export default function RootLayout({}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
