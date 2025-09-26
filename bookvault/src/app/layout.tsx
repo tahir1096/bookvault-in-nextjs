2cd0a0ccfb1098f2dc13c95e3495d8fad1356133
Muhammad Tahir <mtahirkaam@gmail.com>
2025-09-26 20:09:23 +0500

    layout metadata added

--- bookvault/src/app/layout.tsx
+++ bookvault/src/app/layout.tsx
@@ -1,8 +1,18 @@
 import Link from "next/link";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Book Vault",
+  description: "A book management app",
+};  
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
+        <header>
+          <link href="/">Home</link>
+          <link href="/about">Books</link>
+          <link href="/contact">Add Books</link>
+        </header>
         {children}
       </body>
     </html>
