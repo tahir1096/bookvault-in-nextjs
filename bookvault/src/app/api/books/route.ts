5e1bac52fec8c23c368676f6d07519b1cf502900
Muhammad Tahir <mtahirkaam@gmail.com>
2025-09-27 16:46:39 +0500

    get-post

--- bookvault/src/app/api/books/route.ts
+++ bookvault/src/app/api/books/route.ts
@@ -2,3 +2,9 @@ import { books } from "../../../data";
 export async function GET() {
     return Response.json(books);
 }
+export async function POST(request: Request) {
+  const body = await request.json();
+  const newBook = { id: String(Date.now()), ...body };
+  books.push(newBook);
+  return Response.json(newBook, { status: 201 });
+}
