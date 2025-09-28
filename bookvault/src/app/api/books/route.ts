becff2a4965182639a9d2499bbc51f33d6313dd6
Muhammad Tahir <mtahirkaam@gmail.com>
2025-09-28 10:35:47 +0500

    integrate db

--- bookvault/src/app/api/books/route.ts
+++ bookvault/src/app/api/books/route.ts
@@ -1,10 +1,18 @@
-import { books } from "../../../data";
+import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
+
 export async function GET() {
-    return Response.json(books);
+  const client = await clientPromise;
+  const db = client.db();
+  const books = await db.collection("books").find({}).toArray();
+  return Response.json(books);
 }
+
 export async function POST(request: Request) {
   const body = await request.json();
-  const newBook = { id: String(Date.now()), ...body };
-  books.push(newBook);
-  return Response.json(newBook, { status: 201 });
+  const client = await clientPromise;
+  const db = client.db();
+  const newBook = { ...body, createdAt: new Date() };
+  const result = await db.collection("books").insertOne(newBook);
+  return Response.json({ ...newBook, _id: result.insertedId }, { status: 201 });
 }
